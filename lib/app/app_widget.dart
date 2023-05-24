88df09ed2bff185866bd361a5d1136fb27f85987
Júnior Nogueira <frjuniornogueira1@gmail.com>
2023-05-24 16:13:31 -0300

    tela login faltando apenas os textsform

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/database/migrations/sqlite_adm_connection.dart';
 import 'package:todo_list/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list/app/modules/auth/auth_module.dart';
 import 'package:todo_list/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list/app/modules/auth/login/login_page.dart';
@@ -33,6 +34,7 @@ class _AppWidgetState extends State<AppWidget> {
     return MaterialApp(
       title: 'Todo List Provider',
       initialRoute: '/login',
+      theme: TodoListUiConfig.theme,
       routes: {
         ...AuthModule().routers,
       },
--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:todo_list/app/core/widget/todo_list_logo.dart';
 // import 'package:provider/provider.dart';
 // import 'package:todo_list/app/modules/auth/login/login_controller.dart';
@@ -61,6 +63,49 @@ class LoginPage extends StatelessWidget {
                                     ),
                                   ),
                                 )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF0F3F7),
+                          border: Border(
+                            top: BorderSide(
+                              width: 2,
+                              color: Colors.grey.withAlpha(50),
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 30,
+                            ),
+                            SignInButton(
+                              Buttons.Google,
+                              text: 'Continue com o Google',
+                              padding: const EdgeInsets.all(5),
+                              shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              onPressed: () {},
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Não tem conta?'),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text('Cadastre-se'),
+                                ),
                               ],
                             )
                           ],
