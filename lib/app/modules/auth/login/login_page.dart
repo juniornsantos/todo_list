73bf8ffd3f7b86fb5e97b9e5a5854b4572eb31f2
Júnior Nogueira <frjuniornogueira1@gmail.com>
2023-05-23 21:33:38 -0300

    iniciado tela de login

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list/app/modules/auth/login/login_controller.dart';
+import 'package:todo_list/app/core/widget/todo_list_logo.dart';
+// import 'package:provider/provider.dart';
+// import 'package:todo_list/app/modules/auth/login/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<LoginController>(context);
+    // Provider.of<LoginController>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TodoListLogo()
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      body: Container(),
     );
   }
 }
