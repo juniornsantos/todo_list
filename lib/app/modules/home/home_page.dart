3fd037ab2f520b12872b8a0eb5f0b7ebf7fb2669
Junior Nogueira <frjuniornogueira1@gmail.com>
2023-07-13 17:05:19 -0300

    ultima atualizacao

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/auth/auth_provider.dart';
+import 'package:todo_list/app/core/ui/theme_extensions.dart';
+import 'package:todo_list/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list/app/modules/home/widget/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,10 +13,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        iconTheme: IconThemeData(color: context.primaryColor),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: Icon(TodoListIcons.filter),
+            itemBuilder: (_) => [
+              PopupMenuItem<bool>(child: Text('Mostrar tarefas concluídas'))
+            ],
+          )
+        ],
       ),
+      backgroundColor: Color(0XFFFAFBFE),
       drawer: HomeDrawer(),
-      body: Container(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('TEXTO'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
