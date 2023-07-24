1c9cb70dbccbfb3c0fd79ec4edef2148249233b4
Junior Nogueira <frjuniornogueira1@gmail.com>
2023-07-24 07:36:37 -0300

    aula 207 contruindo home e filters

--- lib/app/modules/home/widget/todo_card_filter.dart
+++ lib/app/modules/home/widget/todo_card_filter.dart
@@ -14,7 +14,7 @@ class _TodoCardFilterState extends State<TodoCardFilter> {
     return Container(
       constraints: BoxConstraints(
         minHeight: 120,
-        maxHeight: 150,
+        maxWidth: 150,
       ),
       margin: EdgeInsets.only(
         right: 10,
@@ -24,8 +24,31 @@ class _TodoCardFilterState extends State<TodoCardFilter> {
         color: context.primaryColor,
         border: Border.all(
           width: 1,
-          color: Colors.grey.withOpacity(.8),
+          color: Colors.grey.withOpacity(0.8),
         ),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '10 TASKS',
+            style: context.titleStyle.copyWith(
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            'HOJE',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          LinearProgressIndicator(
+            backgroundColor: context.primaryColorLigth,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            value: 0.4,
+          )
+        ],
       ),
     );
   }
